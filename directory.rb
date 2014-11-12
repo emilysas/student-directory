11af68426db86bafad44e7766ad9d8aa5af29a5f
Emily Sas <emily.bronwen.sas@gmail.com>
2014-11-12 23:11:48 +0000

    ex.9

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ def input_students
       cohort = gets.chomp
       if cohort == "" 
         cohort = :december 
-      else cohort.to_sym
+      else cohort = cohort.to_sym
       end
       print "Please enter #{name}'s country of birth\n"
       country_of_birth = gets.chomp
@@ -24,8 +24,10 @@ def input_students
       @students << {:name => name, :cohort => cohort, :hobbies => hobbies, :country_of_birth => country_of_birth, :height => height}
       print "Now we have #{@students.length} students\n"
       # get another name from the user
-      name = gets.chomp 
+      puts "Please enter another name, or press enter to finish:"
+      name = gets.chomp
     end
+    
   end
   get_info
   # return the array of students
@@ -33,15 +35,17 @@ def input_students
 end
 
 def print_header
-  print "The students of my cohort of Makers Academy\n----------------"
+  print "The students of my cohort of Makers Academy\n----------------\n"
 end
 
 def print_names(names)
   for name in names 
     i = 0
-    while i < names.length
-      puts name
-      i += 1
+    while i < names.length 
+      i += 1 
+      if name[:cohort]==:december 
+        puts name
+      end
     end
   end
 end
