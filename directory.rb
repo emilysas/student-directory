f993be7678013025fdb28153d0e73c30c5e1e1ed
Emily Sas <emily.bronwen.sas@gmail.com>
2014-11-13 12:36:48 +0000

    bug fix

--- directory.rb
+++ directory.rb
@@ -94,7 +94,7 @@ def load_students
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-    @students << {:name => name, :cohort => cohort.sym}
+    @students << {:name => name, :cohort => cohort.to_sym}
   end
   file.close
 end
